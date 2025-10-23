c1b5bd2a828b049fb78606bf03abbbd0765e196f
IssacSegura <issacsegura2005@gmail.com>
2025-10-22 18:29:33 -0600

    initial commit

---
+++ EjercicioPractico1_Biblioteca/src/main/java/com/biblioteca/app/BibliotecaappApplication.java
@@ -0,0 +1,13 @@
+package com.biblioteca.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaappApplication.class, args);
+	}
+
+}
